9ecbb25dfd67581e31e5adfb78ec6ad91d807a3e
unknown <jeniroan11061980@gmail.com>
2020-03-20 17:19:53 +0700

    Belajar php6

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
 <?php 
-$nama depan = "Jenny";
-$nama belakang = "Sabuna";
-	echo $nama_depan ."". $nama_belakang;
 
+$x = 10;
+$x += 2; #operator assignment +=
+	echo $x "<br>";
+$y = 11;
+$y -= 6;
+	echo $y "<br>";#output operator ssignment -=
+$z = 5;
+$z /= 7; #operator assignment /=
+	echo $z "<br>";
+$r = 30;
+$r %= 8;
+	echo $r "<br>";#output operator ssignment %=
+$t = 16;
+$t .= 2;
+	echo $t "<br>";#output operator ssignment .=
 	?>
 
  
