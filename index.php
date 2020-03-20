30331a55bba6f986b83b20742a25a2b397d382b6
unknown <jeniroan11061980@gmail.com>
2020-03-20 17:25:46 +0700

    Belajar php selesai

--- index.php
+++ index.php
@@ -1,20 +1,38 @@
-<?php 
+<?php
+//operator perbandingan
+	var_dump(1 < 5) ;
+		echo "</br>";
 
-$x = 10;
-$x += 2; #operator assignment +=
-	echo $x "<br>";
-$y = 11;
-$y -= 6;
-	echo $y "<br>";#output operator ssignment -=
-$z = 5;
-$z /= 7; #operator assignment /=
-	echo $z "<br>";
-$r = 30;
-$r %= 8;
-	echo $r "<br>";#output operator ssignment %=
-$t = 16;
-$t .= 2;
-	echo $t "<br>";#output operator ssignment .=
-	?>
+	var_dump(1 > 5) ;
+		echo "</br>";
+
+	var_dump(1 <= 5) ;
+		echo "</br>";
+
+	var_dump(1 >= 5) ;
+		echo "</br>";
+
+	var_dump(1 == 5) ;
+		echo "</br>";
+
+	var_dump(1 != 5) ;
+		echo "</br>";
+
+//operator indentitas
+	var_dump(1 === "1");
+		echo "</br>";
+	
+	var_dump(1 !== "1");
+		echo "</br>";
+
+//operator logika
+	$x = 10;
+	var_dump($x < 20 && $x % 2 == 0);
+		echo "</br>";
+	
+	var_dump($x < 20 || $x % 2 == 0);
+		echo "</br>";
+
+?>
 
  
