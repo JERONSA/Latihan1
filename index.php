37169ce600dbfcba52d996a9a612898bceea7826
unknown <jeniroan11061980@gmail.com>
2020-03-20 16:54:24 +0700

    belajar php3

--- index.php
+++ index.php
@@ -1,18 +1,9 @@
-<?php
-// Variabel dan tipe data 
-// Variabel = digunakan untuk menyimpan data
-// Aturan variabel "tidak boleh diawali dengan angka tapi boleh mengandung angka"
+<?php 
 $nama = "Jenny";
-?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>
-		Belajar PHP 
-	</title>
-</head>
-<body>
-	<h1>Hallo, Selamat Datang <?php echo $nama;?></h1>
-</body>
-</html>
+
+	echo "Hallo, Selamat Datang $nama <br>";#petik dua
+	echo 'Hallo, Selamat Datang $nama';#petik satu
+
+	?>
+
  
