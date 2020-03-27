4f9583b417e7fb4c3cf46ca515a3804cbfbee3b8
unknown <jeniroan11061980@gmail.com>
2020-03-27 16:37:51 +0700

    belajar perulangan for 1

---
+++ minggu4/latihan.php
@@ -0,0 +1,13 @@
+<?php
+// pengulangan
+// for 
+for($i=0;$i < 5; $i++){ 
+ 	echo "Hello Word! <br>";
+ } 
+ ?>
+ <!-- 
+ // $i = 0; bagian inisialisasi/menentukan variabel
+ // $i < 5; bagian kondisi/untuk memberhentikan kondisi perulangan ketika berjumlah <5
+ // i++ bagian increment (menambah) kondisi
+ -->
+ 
