a146e2e7eb02c5454ed644e903e49043873df929
unknown <jeniroan11061980@gmail.com>
2020-03-27 18:01:44 +0700

    perulangan do while task5

--- minggu4/latihan.php
+++ minggu4/latihan.php
@@ -1,9 +1,11 @@
 <?php
-
-$ulangi = 1; 
-
-while($ulangi < 6) { 
-	echo "<p>Hai, ini baris ke-$ulangi </p>";
-	$ulangi++; #increment
- } 
- ?>
+// perulangan
+// do while
+	echo "<H2>Menggunakan perulangan Do While</H2>";
+	$i = 10;
+	do {
+	echo "Hello Word! <br>";
+	$i--;
+		} while ($i > 5);
+	# output hanya 1 baris jika While($i<5) namun jika diganti dengan while ($i<5) maka akan tampil tulisan Hello Word sebanyak 5 baris
+	 //  ?>
