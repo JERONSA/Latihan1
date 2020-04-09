3d8ca578a0c7192de770a23d5b2d36d2cf5b344a
unknown <jeniroan11061980@gmail.com>
2020-04-09 19:02:19 +0700

    task 2

--- minggu6/date.php
+++ minggu6/date.php
@@ -1,8 +1,14 @@
 <?php 
 // date untuk menampilkan tanggal dengan format tertentu
-	echo date("l");
+	/*echo date("l");
 	echo date ("d");
 	echo date("M");
 	echo date("m");
-	echo date("Y");	
+	echo date("Y");	*/
+	echo date (
+		"l, d-M-Y",
+		time() +
+		60 * 60 * 24 * 100
+	);	
+	//dengan format tersebut kita dapat mengetahui 100 hari yang akan datang jatuh pada hari apa??
  ?>
