aa1e23123b2eba5fd2644bb7dcbe75bd1a09080b
unknown <jeniroan11061980@gmail.com>
2020-04-09 19:24:18 +0700

    task 6

--- minggu6/date.php
+++ minggu6/date.php
@@ -14,5 +14,9 @@
 	// echo date("l", mktime());
 	//latihan mencari hari tanggal lahir
 
-	echo date ("l", mktime(0,0,0,6,11,1980));
+	//echo date ("l", mktime(0,0,0,6,11,1980));
+
+	//latihan mencari hari ulang tahun pada tahun 2030
+
+	echo date("l", strtotime("11 june 2030"));
  ?>
