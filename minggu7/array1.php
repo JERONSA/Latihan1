b188ad285bc84b3fdd490d37243f3331fa46e00f
unknown <jeniroan11061980@gmail.com>
2020-04-18 13:20:27 +0700

    array dengan data

---
+++ minggu7/array1.php
@@ -0,0 +1,33 @@
+<?php 
+//perulangan pada array
+//for atau foreach
+$angka = [1, 3, 5, 7, 9,];
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- <meta charset="utf-8">
+	<meta name="viewport">
+	<content="width=device-width, initial-scale=1.0"> -->
+	<title>Latihan Array</title>
+	<style>
+		.kotak {
+			width : 50px;
+			height : 50px;
+			background-color: salmon;
+			text-align: center;
+			line-height: 50px;
+			margin : 3px;
+			float : left;
+			}
+	</style>
+</head>
+<body>
+	<div class="kotak">1</div>
+	<div class="kotak">3</div>
+	<div class="kotak">5</div>	
+</body>
+</html>
+
+
