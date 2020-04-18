83da64118cc68bca226b05599c7c4fbff6a99da7
unknown <jeniroan11061980@gmail.com>
2020-04-18 13:54:30 +0700

    contoh perulangan data mahasiswa

---
+++ minggu7/array2.php
@@ -0,0 +1,18 @@
+<?php 
+$mahasiswa = ["Jenny Sabuna", "19520003", "Teknologi Informasi", "jeniroan110061980@gmail.com"];
+ ?>
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>Daftar MAhasiswa</title>
+ </head>
+ <body>
+ <h1>Daftar Mahasiswa</h1>
+ <ul>
+ 	<li>Jenny Sabuna</li>
+ 	<li>19520003</li>
+ 	<li>Teknologi Informasi</li>
+	<li>jeniroan11061980@gmail.com</li>
+ </ul>
+ </body>
+ </html>
