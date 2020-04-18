dcf396056edeb0fe61d65386ca8f44c14ecba566
unknown <jeniroan11061980@gmail.com>
2020-04-18 14:01:29 +0700

    Menggunakan foreach

--- minggu7/array2.php
+++ minggu7/array2.php
@@ -9,10 +9,12 @@ $mahasiswa = ["Jenny Sabuna", "19520003", "Teknologi Informasi", "jeniroan110061
  <body>
  <h1>Daftar Mahasiswa</h1>
  <ul>
- 	<li>Jenny Sabuna</li>
- 	<li>19520003</li>
- 	<li>Teknologi Informasi</li>
-	<li>jeniroan11061980@gmail.com</li>
+ 	<?php foreach ($mahasiswa as $mhs): ?>
+ 		<li>
+ 			<?php echo $mhs; ?>
+ 		</li>
+	<?php endforeach; ?>
+ 		
  </ul>
  </body>
  </html>
