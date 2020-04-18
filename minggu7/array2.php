e9b7ca07ce0c40fafdad8053a21b34fe68e1a3da
unknown <jeniroan11061980@gmail.com>
2020-04-18 14:41:00 +0700

    menampilkan semua data mahasiswa berdasarkan indeks

--- minggu7/array2.php
+++ minggu7/array2.php
@@ -10,6 +10,9 @@ $mahasiswa = ["Jenny Sabuna", "19520003", "Teknologi Informasi", "jeniroan110061
  <h1>Daftar Mahasiswa</h1>
  <ul>
  	<li><?php echo "$mahasiswa[0]"; ?></li>	
+ 	<li><?php echo "$mahasiswa[1]"; ?></li>	
+ 	<li><?php echo "$mahasiswa[2]"; ?></li>	
+ 	<li><?php echo "$mahasiswa[3]"; ?></li>	
  </ul>
  </body>
  </html>
