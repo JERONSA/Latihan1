76fab67456924a473508260da6280471b83194eb
unknown <jeniroan11061980@gmail.com>
2020-04-18 14:57:51 +0700

    Menampilkan data berdasar indeks

--- minggu7/array2.php
+++ minggu7/array2.php
@@ -1,18 +1,23 @@
 <?php 
-$mahasiswa = ["Jenny Sabuna", "19520003", "Teknologi Informasi", "jeniroan110061980@gmail.com"];
+//array multidimensi/array didalam array/array bersarang
+$mahasiswa = [
+	 ["Jenny Sabuna", "19520003", "Teknologi Informasi", "jeniroan110061980@gmail.com"], 
+ 	["Anggi", "19510003", "Sistem Informasi", "anggi@gmail.com"]
+];
  ?>
  <!DOCTYPE html>
  <html>
  <head>
- 	<title>Daftar MAhasiswa</title>
+ 	<title>Daftar Mahasiswa</title>
  </head>
  <body>
  <h1>Daftar Mahasiswa</h1>
  <ul>
- 	<li><?php echo "$mahasiswa[0]"; ?></li>	
- 	<li><?php echo "$mahasiswa[1]"; ?></li>	
- 	<li><?php echo "$mahasiswa[2]"; ?></li>	
- 	<li><?php echo "$mahasiswa[3]"; ?></li>	
+ 	<li><?php echo $mahasiswa[0][0]; ?></li>	
+ 	<li><?php echo $mahasiswa[0][1]; ?></li>	
+ 	<li><?php echo $mahasiswa[0][2]; ?></li>	
+ 	<li><?php echo $mahasiswa[0][3]; ?></li>
+
  </ul>
  </body>
  </html>
