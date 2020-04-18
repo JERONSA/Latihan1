39666942f279d0f6066a476caedbbafa382d40cf
unknown <jeniroan11061980@gmail.com>
2020-04-18 12:15:26 +0700

    manambahkan array

--- minggu7/array.php
+++ minggu7/array.php
@@ -8,9 +8,18 @@ $hari = array("Senin", "Selasa", "Rabu");
 
 //echo $hari [0]
 
-//print_r($hari);
-
+// print_r($hari);
+// echo "<br>";
 //cara baru
 $bulan = ["Januari", "Februari", "Maret"];
 print_r($bulan);
+
+var_dump($hari);
+$hari [] = "Kamis";
+echo "<br>";
+var_dump($hari);
+
+
+
+
  ?>}
