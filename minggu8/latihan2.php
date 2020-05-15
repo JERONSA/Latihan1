4e6ff9e83c47272e9d4397b75ec8980d9daa4dbc
unknown <jeniroan11061980@gmail.com>
2020-05-16 07:58:41 +0800

    membuat style css

---
+++ minggu8/latihan2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Latihan Array</title>
+	<style>
+		.kotak {
+			width: 30px;
+			height: 30px;
+			background-color: greenyellow;
+			vertical-align: center;
+			line-height: 30px;
+			margin: 3px;
+			float: left;
+		}
+	</style>
+</head>
+<body>
+	<div class="kotak"></div>
+</body>
+</html>
