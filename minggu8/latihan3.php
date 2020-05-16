8c39147339d810a819b27c1a25d473a028fa5fc1
unknown <jeniroan11061980@gmail.com>
2020-05-16 15:10:09 +0800

    Tugas Array Multidimensi

--- minggu8/latihan3.php
+++ minggu8/latihan3.php
@@ -5,14 +5,16 @@
  "nim" => "19520003",
  "jurusan" => "Teknologi Informasi", 
  "email" => "jeniroan11061980@gmail.com",
-  "tugas" => [100,90,80]
+ "gambar" => "jenny.jpg"
+  // "tugas" => [100,90,80]
 ],
 
 [
  "nama" =>"Adolfina Sabuna", 
  "nim" =>"19520001", 
  "jurusan" => "Teknologi Informasi", 
- "email" =>"jeniroan@gmail.com"
+ "email" =>"jeniroan@gmail.com",
+ "gambar" => "siren.jpg"
 	]
 ];
   ?>
@@ -23,19 +25,26 @@
   </head>
   <body>
   	<h1>Data Mahasiswa</h1>
-  	<?php echo $mahasiswa[0]["tugas"][1]; ?>
-  </body>
 
+<?php foreach ($mahasiswa as $mhs): ?>
+  	<ul>
+  		<li> 
+  		<img src="img/<?= $mhs["gambar"]; ?>">
+  			
+  		</li>
+  		<li>Nama : <?= $mhs["nama"] ?></li> 
+  		<li>NIM :<?= $mhs["nim"] ?></li>
+  		<li>Jurusan :<?= $mhs["jurusan"] ?></li>
+  		<li>Email : <?= $mhs["email"] ?></li> 
+  	</ul> 
+  <?php endforeach; ?> 
+  	  </body>
   </html>
 
 
+  <!-- <?php echo $mahasiswa[0]["tugas"][1]; ?> -->
 
 
-  <!-- <?php foreach ($mahasiswa as $mhs): ?> --> 
-  	<!-- <ul> -->
-  		<!-- <li>Nama : <?= $mhs["nama"] ?></li> -->
-  		<!-- <li>NIM :<?= $mhs["nim"] ?></li> -->
-  		<!-- <li>Jurusan :<?= $mhs["jurusan"] ?></li> -->
-  		<!-- <li>Email : <?= $mhs["email"] ?></li> -->
-  	<!-- </ul>   -->
-  <!-- <?php endforeach; ?> --> 
+
+
+ 
---
+++ minggu8/latihan3tugas.php
@@ -0,0 +1,142 @@
+ <?php 
+ $sepatu = [
+     ["merk" => "Jimmy Choo", 
+     "harga" => "$695",
+     "pengiriman" => "Luar Negeri", 
+     "variation" => "[36.37.38.39,40]",
+     "pembayaran" => "Bisa COD",
+     "gambar" => "sepatu1.jpg"
+    ],
+    ["merk" => "Charlotte Olympia", 
+     "harga" => "$525",
+     "pengiriman" => "Jakarta", 
+     "variation" =>  "[36.37.38.39,40]",
+     "pembayaran" => "Kartu Kredit",
+     "gambar" => "sepatu2.jpg"
+    ],
+     ["merk" => "Christian Louboutin", 
+     "harga" => "$426",
+     "pengiriman" => "Bandung", 
+     "variation" =>  "[36.37.38.39,40]",
+     "pembayaran" => "Transfer",
+     "gambar" => "sepatu3.jpg"
+    ],
+     ["merk" => "Manolo Blahnik", 
+     "harga" => "$426",
+     "pengiriman" => "Surabaya", 
+     "variation" =>  "[36.37.38.39,40]",
+     "pembayaran" => "Bisa COD",
+     "gambar" => "sepatu4.jpg"
+    ],
+     ["merk" => "Walter Steiger", 
+     "harga" => "$625",
+     "pengiriman" => "Luar Negeri", 
+     "variation" =>  "[36.37.38.39,40]",
+     "pembayaran" => "Kartu Kredit",
+     "gambar" => "sepatu5.jpg"
+    ],
+     ["merk" => "Guiseppe Zanoti", 
+     "harga" => "$395",
+     "pengiriman" => "Jakarta", 
+     "variation" =>  "[36.37.38.39,40]",
+     "pembayaran" => "Transfer",
+     "gambar" => "sepatu6.jpg"
+    ],
+     ["merk" => "Stuart Weitzman", 
+     "harga" => "$523",
+     "pengiriman" => "Luar Negeri", 
+     "variation" => "[36.37.38.39,40]",
+     "pembayaran" => "Transfer",
+     "gambar" => "sepatu7.jpg"
+    ],
+     ["merk" => "Salvatore Ferragamo", 
+     "harga" => "$295",
+     "pengiriman" => "Jakarta", 
+     "variation" =>  "[36.37.38.39,40]",
+     "pembayaran" => "Bisa COD",
+     "gambar" => "sepatu8.jpg"
+    ],
+     ["merk" => "Brian Atwood", 
+    "harga" => "$230",
+     "pengiriman" => "Surabaya", 
+     "variation" =>  "[36.37.38.39,40]",
+     "pembayaran" => "Transfer",
+     "gambar" => "sepatu9.jpg"
+    ],
+     ["merk" => "Converse", 
+     "harga" => "Rp.1.952.000",
+     "pengiriman" => "Bandung", 
+     "variation" =>  "[36.37.38.39,40]",
+     "pembayaran" => "Transfer",
+     "gambar" => "sepatu10.jpg"
+    ],
+     ["merk" => "Hush Puppies", 
+     "harga" => "Rp.175.000",
+     "pengiriman" => "Bandung", 
+     "variation" =>  "[36.37.38.39,40, 41]",
+     "pembayaran" => "Transfer",
+     "gambar" => "sepatu11.jpg"
+    ],
+     ["merk" => "Louis Vuitton", 
+    "harga" => "Rp.1.300.000",
+     "pengiriman" => "Jogyakarta", 
+     "variation" =>  "[36.37.38.39,40]",
+     "pembayaran" => "Transfer",
+     "gambar" => "sepatu12.jpg"
+    ],
+    [ "merk" => "PVRA",
+     "harga" => "Rp.1.250.000",
+     "pengiriman" =>"Malang",
+     "variation" => "[36.37.38.39,40]",
+     "pembayaran" => "Transfer",
+     "gambar" => "sepatu13.jpg"
+    ],
+     [
+      "merk" => "Adidas Yeezy", 
+    "harga" => "Rp.952.000",
+     "pengiriman" => "Surabaya", 
+     "variation" =>  "[36.37.38.39,40]",
+     "pembayaran" => "Transfer",
+     "gambar" => "sepatu14.jpg"
+    ],
+    [
+     "merk" =>"Reebook", 
+    "harga" => "Rp.695.000",
+     "pengiriman" => "Jakarta", 
+     "variation" =>  "[36.37.38.39,40]",
+     "pembayaran" => "Transfer",
+     "gambar" => "sepatu15.jpg",
+      ],
+  ];
+
+  ?>
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <title>Katalog</title>
+  </head>
+<body>
+    <h1>KATALOG SEPATU WANITA BRANDED</h1>
+     
+    <?php foreach ($sepatu as $spt): ?>
+     <ul>
+        <li> 
+          <img src="img/<?= $spt["gambar"]; ?>">
+        </li>
+        <li>MERK : <?= $spt["merk"] ?></li> 
+        <li>HARGA : <?= $spt["harga"] ?></li>
+        <li>PENGIRIMAN : <?= $spt["pengiriman"] ?></li>
+        <li>VARIATION : <?= $spt["variation"] ?></li> 
+        <li>PEMBAYARAN : <?=$spt["pembayaran"] ?></li>
+      </ul> 
+  <?php endforeach; ?> 
+ </body>
+</html>
+
+
+  
+
+
+
+
+ 
