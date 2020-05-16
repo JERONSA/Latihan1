004306450b5fffb0b209d0de64c6430375844c47
unknown <jeniroan11061980@gmail.com>
2020-05-16 09:55:58 +0800

    Array Multidimensi1

---
+++ minggu8/latihan3.php
@@ -0,0 +1,20 @@
+ <?php 
+//array multi dimensi
+ $mahasiswa = ["Jenny Sabuna", "19520003", "Teknologi Informasi", "jeniroan11061980@gmail.com"]
+  ?>
+  <!DOCTYPE html>
+  <html>
+  <head>
+  	<title>Data MAhasiswa</title>
+  </head>
+  <body>
+  	<h1>Data Mahasiswa</h1>
+  	<ul>
+  		<li><?=$mahasiswa[0] ?></li>
+  		<li><?=$mahasiswa[1] ?></li>
+  		<li><?=$mahasiswa[2] ?></li>
+  		<li><?=$mahasiswa[3] ?></li>
+  	</ul>  
+  </body>
+
+  </html>
