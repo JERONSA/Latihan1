5cd4162c96980d8a49a50ad25cbd713e8d5a72b6
unknown <jeniroan11061980@gmail.com>
2020-06-06 11:06:33 +0800

    membuat link1

---
+++ minggu9/latihan1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Detail Sepatu</title>
+</head>
+<body>
+<ul>
+	<li><img src="img/sepatu1.jpg"></li>
+	<li>Jimmy Choo</li>
+	<li>$695</li>
+	<li>Luar Negeri</li>
+	<li>[36.37.38.39.40]</li>
+	<li>Bisa COD</li>
+</ul>
+</body>
+</html>
