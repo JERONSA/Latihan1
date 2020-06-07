3b9fd21fdaa9f65c566684ffa0664cff6dad8813
unknown <jeniroan11061980@gmail.com>
2020-06-07 11:21:00 +0800

    form login dengan username dan password

--- minggu9/login.php
+++ minggu9/login.php
@@ -12,6 +12,10 @@
 			<label for="username">Username : </label>
 			<input type="text" name="username" id="username">
 		</li>
+		<li>
+			<label for="password">Pasword : </label>
+			<input type="password" name="password" id="password">
+		</li>
 	</form>
 </ul>
 </body>
