483c01ff6bcefdb56833fcb3464c4fa36b634132
unknown <jeniroan11061980@gmail.com>
2020-06-07 11:17:28 +0800

    form login dengan label for

---
+++ minggu9/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Halaman Login</title>
+</head>
+<body>
+<h1>Login Admin</h1>
+
+<ul>
+	<form action="" method="POST">
+		<li>
+			<label for="username">Username : </label>
+			<input type="text" name="username" id="username">
+		</li>
+	</form>
+</ul>
+</body>
+</html>
