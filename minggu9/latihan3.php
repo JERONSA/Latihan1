ece1ed7eafb2c4946c3e73412b2ac0b860b8bbf6
unknown <jeniroan11061980@gmail.com>
2020-06-07 09:57:56 +0800

    membuat form login sederhana

---
+++ minggu9/latihan3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Metode Post</title>
+</head>
+<body>
+	<form action="latihan2.php" method="Post">
+		Masukkan Nama :
+	<input type="text" name="nama">	
+	<br>
+	<button type="submit" name="submit">
+		Kirim!
+	</button>
+	</form>
+</body>
+</html>
